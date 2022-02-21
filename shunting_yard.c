7e064e908c026da60b5401ce910c577274e31556
dreivmeister <felix.brandt@fh-bielefeld.de>
2022-02-21 19:25:28 +0100

    RPN SY

--- shunting_yard.c
+++ shunting_yard.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 struct parser {
-    struct queue* input;
+    struct stack* input;
     struct queue* output;
     struct stack* st;
 };
@@ -21,14 +21,14 @@ struct token {
 };
 
 
-struct parser* newParser(struct queue* inp, int size, int cap) {
+struct parser* newParser(struct stack* inp, int size, int cap) {
     struct parser* pa = (struct parser*) malloc(sizeof(struct parser));
 
     pa->st = newStack(cap);
-    pa->input = newQueue(size);
+    pa->input = newStack(size);
     pa->output = newQueue(size);
     pa->input = inp;
-
+    
     return pa;
 }
 
@@ -53,38 +53,32 @@ int associativity(struct token o) {
 
 
 void shuntingyard(struct parser* pa) {
-    int numelem = sizeQ(pa->input);
-    printf("sy\n");
+    int numelem = sizeS(pa->input);
     int currIndex = 0;
     while(currIndex < numelem) {
-        printf("im sy\n");
-        struct token currToken = front(pa->input);
-        dequeue(pa->input);
-
+        struct token currToken = pop(pa->input);
+        
         if(currToken.isNumber) {
-            printf("enqueue sy\n");
             enqueue(pa->output, currToken);
         }
         else if(currToken.isFunction) {
             push(pa->st, currToken);
         }
         else if(currToken.isOperator) { //o1
-            printf("isOperator\n");
-            struct token topst = peek(pa->st); //top operator stack (o2)
-            //stack not empty && top of stack greater precedence than current || equal precedence && current left associative
-            while(strcmp("(", topst.value) != 0  && (precedence(topst, currToken) > 0 || 
-                 (precedence(topst, currToken) == 0 && associativity(currToken) > 0))) {
+            struct token topst = peek(pa->st); //o2
+            while(strcmp(")", topst.value) != 0  && (precedence(topst, currToken) > 0 || 
+                 (precedence(topst, currToken) == 0 && associativity(currToken) < 0))) {
                      enqueue(pa->output, topst);
                      pop(pa->st);
             }
             enqueue(pa->output, currToken);
         }
-        else if(strcmp("(", currToken.value) == 0) {
+        else if(strcmp(")", currToken.value) == 0) {
             enqueue(pa->output, currToken);
         }
-        else if(strcmp(")", currToken.value) == 0) {
+        else if(strcmp("(", currToken.value) == 0) {
             struct token topst = peek(pa->st); //top operator stack (o2)
-            while(strcmp("(", topst.value) != 0) {
+            while(strcmp(")", topst.value) != 0) {
                 if(isEmptyS(pa->st)) {
                     printf("Error: Mismatched Parentheses.\n");
                     return;
@@ -92,7 +86,7 @@ void shuntingyard(struct parser* pa) {
                 enqueue(pa->output, topst);
                 pop(pa->st);
             }
-            if(strcmp("(", topst.value) == 0) {
+            if(strcmp(")", topst.value) == 0) {
                 pop(pa->st);
             }
             if(peek(pa->st).isFunction) {
@@ -105,7 +99,7 @@ void shuntingyard(struct parser* pa) {
     }
     while(!isEmptyS(pa->st)) {
         struct token topst = peek(pa->st);
-        if(strcmp("(", topst.value) == 0) {
+        if(strcmp(")", topst.value) == 0) {
             printf("Error: Mismatched Parentheses.\n");
             return;
         }
